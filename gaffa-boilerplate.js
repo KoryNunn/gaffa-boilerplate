a9ccd39371dc0b2c2a9c7663432050443b391553
Kory Nunn <knunn187@gmail.com>
2013-09-28 12:44:32 +1000

    improved boilerplate to npm install and browserify

--- gaffa-boilerplate.js
+++ gaffa-boilerplate.js
@@ -3,6 +3,7 @@
 var program = require('commander'),
     path = require('path'),
     fs = require('fs-extra'),
+    exec = require("child_process").exec,
     fromPath = path.join(__dirname, 'boilerplate'),
     packageJson = require('./package.json');
 
@@ -20,6 +21,12 @@ fs.copy(fromPath, process.cwd(), function (err) {
   if (err) {
     console.error(err);
   } else {
-    console.log("success!");
+    console.log("Files coppied, installing modules");
+    exec('npm --prefix ./scripts install', function(){
+        console.log("Modules installed, browserifying app.js");
+        exec('browserify ./scripts/app.js > ./scripts/app.browser.js', function(){
+            console.log("success! open index.html in a browser.");
+        }).stdout.pipe(process.stdout);
+    }).stdout.pipe(process.stdout);;
   }
 });
