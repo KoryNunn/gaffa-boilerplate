bd9a67e3bf97788ca9bf71feb408ad23e0011e14
Kory Nunn <knunn187@gmail.com>
2013-10-01 11:10:03 +1000

    improved to now start a watcher

--- gaffa-boilerplate.js
+++ gaffa-boilerplate.js
@@ -3,7 +3,9 @@
 var program = require('commander'),
     path = require('path'),
     fs = require('fs-extra'),
-    exec = require("child_process").exec,
+    childProcess = require("child_process"),
+    exec = childProcess.exec,
+    spawn = childProcess.spawn,
     fromPath = path.join(__dirname, 'boilerplate'),
     packageJson = require('./package.json');
 
@@ -11,22 +13,43 @@ program._name = packageJson.name;
 program
     .version(packageJson.version)
     .option('-s, --scriptsOnly', 'Only boilerplate JS, not HTML')
+    .option('-b, --browserify', 'Only set up browserify watcher')
     .parse(process.argv);
 
 if(program.scriptsOnly){
     fromPath = path.join(fromPath, 'scripts');
 }
 
-fs.copy(fromPath, process.cwd(), function (err) {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log("Files coppied, installing modules");
+function browserifyer(){
+    exec('browserifyer -w ./ -i ./app.js -o ./app.browser.js -v', {
+        cwd: program.scriptsOnly ? './' : './scripts'
+    }).stdout.pipe(process.stdout);
+}
+
+function install(){
     exec('npm --prefix ./scripts install', function(){
         console.log("Modules installed, browserifying app.js");
-        exec('browserify ./scripts/app.js > ./scripts/app.browser.js', function(){
-            console.log("success! open index.html in a browser.");
-        }).stdout.pipe(process.stdout);
-    }).stdout.pipe(process.stdout);;
-  }
-});
+        browserifyer();
+    }).stdout.pipe(process.stdout);
+}
+
+if(program.browserify){
+    browserifyer();
+}else{
+
+    fs.exists(path.join(fromPath, program.scriptsOnly ? 'app.js' : 'index.html'), function(exists){
+        if(exists){
+            console.warn('Warning: this directory contains files that would have been overwritten! Just starting browserifyer instead.');
+            browserifyer();
+        }else{
+            fs.copy(fromPath, process.cwd(), function (error) {
+                if (error) {
+                    console.error(error);
+                } else {
+                    console.log("Files coppied, installing modules");
+                    install();
+                }
+            });
+        }
+    });
+}
